8265abda22d3e16be380599b7e31f03c9c3fd74a
onion27 <ude0721juji@icloud.com>
2022-08-18 22:39:03 +0900

    feat; add chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ChakraProvider>
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   )
 }
 
